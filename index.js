56f01943b971d87dc30b081e35e074c6f6e43c2b
Zsolt Szalma <zsolt.szalma@gmail.com>
2022-08-01 19:24:02 +0200

    Set up Express server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Sidewalk Astronomy')
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res, next) => {
+  res.send('Sidewalk Astronomy API')
+});
+
+app.listen(port, '127.0.0.1', console.log(`Server is listening on PORT ${port}...`));
