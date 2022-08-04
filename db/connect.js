a0435779fe54fe286d934da3650338dd7027d8ce
Zsolt Szalma <zsolt.szalma@gmail.com>
2022-08-04 11:39:56 +0200

    Setup dotenv for DB env vars

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-module.exports = () => {
-  const DBNAME = 'sidewalk-astro';
-  const connectionString = 'mongodb+srv://sidewalkastro:1Galagonyas6@sidewalk-astro.1gq9a.mongodb.net/' + 'sidewalk-astro?retryWrites=true&w=majority';
-  return mongoose.connect(connectionString)
+module.exports = (connectionStr) => {
+  return mongoose.connect(connectionStr)
 };
 
  
--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const app = express();
+
+// this puts the content of the .env file to the environment variables
+require('dotenv').config();
+
 const connectDB = require('./db/connect');
 
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res, next) => {
-  res.send('Sidewalk Astronomy API');
-});
-
 const startApp = async () => {
+
+  const { 
+    MONGODB_USERNAME,
+    MONGODB_PASSWORD,
+    MONGODB_URI,
+    MONGODB_DB_NAME
+  } = process.env;
+
   try {
-    await connectDB();
-    console.log('Connected to MongoDB remote server...')
+    await connectDB(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_URI}/${MONGODB_DB_NAME}?retryWrites=true&w=majority`);
+    console.log('Connected to MongoDB remote server...');
     app.listen(port, '127.0.0.1', console.log(`WebServer is listening on PORT ${port}...`));
     app.use('/api/v1/events', require('./routes/events'));
   } catch (error) {
