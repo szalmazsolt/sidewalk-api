6fb516b0ca7780eea80ba7a754adac21ad99cddb
Zsolt Szalma <zsolt.szalma@gmail.com>
2022-08-04 11:13:47 +0200

    Refactor DB connection function

--- db/connect.js
+++ db/connect.js
@@ -3,12 +3,7 @@ const mongoose = require('mongoose');
 module.exports = () => {
   const DBNAME = 'sidewalk-astro';
   const connectionString = 'mongodb+srv://sidewalkastro:1Galagonyas6@sidewalk-astro.1gq9a.mongodb.net/' + 'sidewalk-astro?retryWrites=true&w=majority';
-  return () => {
-    return mongoose
-      .connect(connectionString)
-      .then(() => console.log('Successfully connected to MongoDB...'))
-      .catch(err => console.log('Unable to connect to MongoDB...'))
-  }
+  return mongoose.connect(connectionString)
 };
 
  
--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect');
 
 app.use(express.json());
 
-
 const port = process.env.PORT || 5000;
 
-app.use('/api/v1/events', require('./routes/events'));
-
 app.get('/', (req, res, next) => {
-  res.send('Sidewalk Astronomy API')
+  res.send('Sidewalk Astronomy API');
 });
-
-const connectDB = require('./db/connect')();
 
 const startApp = async () => {
   try {
     await connectDB();
-    app.listen(port, '127.0.0.1', console.log(`Server is listening on PORT ${port}...`));
+    console.log('Connected to MongoDB remote server...')
+    app.listen(port, '127.0.0.1', console.log(`WebServer is listening on PORT ${port}...`));
+    app.use('/api/v1/events', require('./routes/events'));
   } catch (error) {
-    console.log('An error occured. App could not be started.', error)
+    console.log('An error occured. App could not be started.', error.message);
   }
 };
 
